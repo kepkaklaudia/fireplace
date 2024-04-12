242736687669d8248f2a45f43b4f34cfe0e366a8
kepkaklaudia@gmail.com <kepkaklaudia@gmail.com>
2024-04-12 12:12:00 +0200

    distribution added

---
+++ app/components/Main/distribution.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Image from "next/image";
+
+export const Disctribution = () => (
+  <section className="flex flex-col bg-[url('../public/cover.jpg')] bg-cover bg-center h-[500px]">
+    <p className="text-white text-center text-2xl lg:text-3xl mt-10 mb-6 text-semibold">
+      Where to buy?
+    </p>
+    <p className="text-white text-center text-2xl lg:text-3xl mt-10 mb-6">
+      {" "}
+      Our official distributor is{" "}
+    </p>
+    <div className="flex  gap-8 justify-center">
+      <a
+        href="https://kratki.com/pl"
+        rel="noopener noreferrer"
+        target="_blank"
+        className="focus-style-white"
+      >
+        <Image
+          alt=""
+          width={500}
+          height={500}
+          className=" w-auto max-h-[80px] md:max-h-[100px] p-1"
+          src="/kratki.webp"
+        />
+      </a>
+    </div>
+  </section>
+);
