065c122c27452135ccea194c597966d41169aca0
kepkaklaudia@gmail.com <kepkaklaudia@gmail.com>
2024-11-01 12:12:00 +0100

    footer added

---
+++ app/footer.tsx
@@ -0,0 +1,18 @@
+export const Footer = () => {
+  return (
+    <>
+      <footer className="bg-white w-full">
+        <div className="">
+          <hr className=" border-gray-200 sm:mx-auto" />
+          <span className="block text-sm text-center my-6 lg:my-8">
+            © 2024{" "}
+            <a href="https://ildnord.com/" className="hover:text-red">
+              ILDNORD
+            </a>
+            . All Rights Reserved
+          </span>
+        </div>
+      </footer>
+    </>
+  );
+};
