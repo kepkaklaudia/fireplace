0b66ddee7ada8bbaf676c984a93d8674b3a5a54b
kepkaklaudia@gmail.com <kepkaklaudia@gmail.com>
2024-11-12 12:12:00 +0100

    footer refactoring, tracking added

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,21 +1,17 @@
-export const Footer = () => {
-  return (
-    <>
-      <footer className="bg-white w-full">
-        <div className="">
-          <hr className=" border-gray-200 sm:mx-auto" />
-          <span className="block text-sm text-center my-6 lg:my-8">
-            © 2024{" "}
-            <a
-              href="https://ildnord.com/"
-              className="hover:text-red focus-style-blue py-0.5"
-            >
-              ILDNORD
-            </a>
-            . All Rights Reserved
-          </span>
-        </div>
-      </footer>
-    </>
-  );
-};
+export const Footer = () => (
+  <footer className="bg-white w-full tracking-wide">
+    <div className="">
+      <hr className=" border-gray-200 sm:mx-auto" />
+      <span className="block text-sm text-center my-5 lg:my-6">
+        © 2024{" "}
+        <a
+          href="https://ildnord.com/"
+          className="hover:text-red focus-style-blue py-0.5"
+        >
+          ILDNORD
+        </a>
+        . All Rights Reserved
+      </span>
+    </div>
+  </footer>
+);
